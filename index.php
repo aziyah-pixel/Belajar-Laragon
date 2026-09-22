d5dfad4003c1b6a7010c69f1ecf2d185071aa5a1
Nanda Nur Aziyah <aziyahnanda6@gmail.com>
2026-09-22 10:39:05 +0700

    if dengan from

--- index.php
+++ index.php
@@ -1,25 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-$nama = "Nanda";
-$jurusan = "Informatika";
-$kampus = "ITB AAS Indonesia";
+        $nama = "Nanda";
+        $jurusan = "Informatika";
+        $kampus = "ITB AAS Indonesia";
 
-echo "Nama saya: " . $nama . "<br>";
-echo "Jurusan: " . $jurusan . "<br>";
-echo "Kampus: " . $kampus;
+        echo "Nama saya: " . $nama . "<br>";
+        echo "Jurusan: " . $jurusan . "<br>";
+        echo "Kampus: " . $kampus . "<br>";
+    ?>
+    <form action="" method="post">
+        <label for="">Nilai</label>
+        <input type="number" name="nilai" id="">
+        <button type="submit"></button>
+    </form>
+    <br><br>
 
-//pengunaan if else
-$nilai = 85;
+    <?php
+        //pengunaan if else
+       if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-if ($nilai >= 75) {
-    echo "Nama: " . $nama . "<br>";
-    echo "Nilai: " . $nilai . "<br>";
-    echo "Status: LULUS";
-} else {
-    echo "Nama: " . $nama . "<br>";
-    echo "Nilai: " . $nilai . "<br>";
-    echo "Status: BELUM LULUS";
-}
+        $nilai = $_POST['nilai'];
+        
+        if ($nilai >= 75) {
+            echo "Nilai: " . $nilai . "<br>";
+            echo "Status: LULUS";
+        } else {
+            echo "Nilai: " . $nilai . "<br>";
+            echo "Status: BELUM LULUS";
+        }
 
-
-?>
+       }
+    ?>
+    
+</body>
+</html>
