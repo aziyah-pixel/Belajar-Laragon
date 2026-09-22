0ba18b71eae5bb714bf8166ece04bd23e9490275
Nanda Nur Aziyah <aziyahnanda6@gmail.com>
2026-09-22 11:38:25 +0700

    koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$dbname = "mahasiswa";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Koneksi database berhasil!";
+
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+}
