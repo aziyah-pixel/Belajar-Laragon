0a4832ad1c82dac1566c99d3e61d8a41976c70b2
Nanda Nur Aziyah <aziyahnanda6@gmail.com>
2026-09-22 10:27:01 +0700

    pengunaan ef else

--- index.php
+++ index.php
@@ -8,4 +8,18 @@ echo "Nama saya: " . $nama . "<br>";
 echo "Jurusan: " . $jurusan . "<br>";
 echo "Kampus: " . $kampus;
 
+//pengunaan if else
+$nilai = 85;
+
+if ($nilai >= 75) {
+    echo "Nama: " . $nama . "<br>";
+    echo "Nilai: " . $nilai . "<br>";
+    echo "Status: LULUS";
+} else {
+    echo "Nama: " . $nama . "<br>";
+    echo "Nilai: " . $nilai . "<br>";
+    echo "Status: BELUM LULUS";
+}
+
+
 ?>
