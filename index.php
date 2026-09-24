cac35dee07f12b080feda76b734fc137b75aa845
Nanda Nur Aziyah <aziyahnanda6@gmail.com>
2026-09-24 09:07:32 +0700

    kerangka_tampilan

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid p-3 bg-primary text-white text-center">
+  <h1>DATA MAHASISWA</h1>
+</div>
+  
+<div class="container mt-5 bg-red">
+<button type="button" class="btn btn-success mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Tambah</button>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Tambah Mahasiswa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form>
+            <div class="mb-3">
+                <label for="recipient-name" class="col-form-label">Nama:</label>
+                <input type="text" class="form-control" id="nama">
+            </div>
+            <div class="mb-3">
+                <label for="message-text" class="col-form-label">Alamat :</label>
+                <textarea class="form-control" id="alamat"></textarea>
+            </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Send message</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <table class="table table-striped">
+        <th>NIM</th>
+        <th>NAMA</th>
+        <th>ALAMAT</th>
+    </table>
+</div>
+
+</body>
+</html>
